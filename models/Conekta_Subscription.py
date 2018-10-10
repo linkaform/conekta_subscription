1f2e1560cabba515da40ff713d3f0d516625db3e
=Erickhillo <=>
2018-10-10 09:59:43 -0500

    reconciliacion de pagos

--- models/Conekta_Subscription.py
+++ models/Conekta_Subscription.py
@@ -37,25 +37,41 @@ class ConektaSubscriptions(models.Model):
 
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
+    error = fields.Char()
+
+    @api.multi
+    def action_invoice_paid(self):
+        res = super(AccountInvoice, self).action_invoice_paid()
+        return res
+
 
     def action_invoice_open(self):
         subscription = self.invoice_line_ids.subscription_id.id
         auto_pay = self.invoice_line_ids.subscription_id.auto_pay
+        card = self.invoice_line_ids.subscription_id.cards_conekta
 
         if subscription != False and auto_pay != False:
             values = super(AccountInvoice, self).action_invoice_open()
             if self.number :
                 res = self.conekta_payment_validate()
                 if res == True:
+                    pay = self._create_payment()
+                    invoice = self.search_factura(self.id)
+                    reconcile = self.reconcile_invoice_with_payment(invoice,pay.id)
                     trans = self._create_payment_transaction()
-                    # pay.payment_transaction_id = trans.id
-                    # trans.state = 'done'
+
                 else:
                     message = 'Message form your friends at Contekta \n'+self.error
                     raise ValidationError(message)
         else:
            res = super(AccountInvoice, self).action_invoice_open()
         return False
+
+    @api.model
+    def search_factura(self,invoice_id):
+        invoice_brw = self.env['account.invoice'].browse(invoice_id)
+        return invoice_brw
+
 
 
     def _set_conketa_key(self):
@@ -75,6 +91,7 @@ class AccountInvoice(models.Model):
 
         return True
 
+    @api.model
     def _create_payment_transaction(self):
         self.payment_model = self.env['payment.transaction']
         transaction_model = {
@@ -92,23 +109,39 @@ class AccountInvoice(models.Model):
         # print 'transaction', transaction
         return transaction
 
-    # def _create_payment(self):
-    #     self.payment_env = self.env['account.payment']
-    #     # payment_model = {
-    #     #         'payment_type': 'inbound',
-    #     #         'partner_type': 'customer',
-    #     #         'partner_id': self.partner_id.id,
-    #     #         'amount': self.amount_total,
-    #     #         'currency_id': self.currency_id.id,
-    #     #         'l10n_mx_edi_payment_method_id': self.l10n_mx_edi_payment_method_id.id,
-    #     #         'communication': self.number
-    #     #     }
-    #     # print 'paaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaymeeeeeeeeeeeeeeeeeeeeeeeeeeent', payment_model
-    #     print '****************************************DIR*******************************', dir(self.payment_env)
+    @api.model
+    def reconcile_invoice_with_payment(self, invoice_brw, payment_id):
+        payment_credit_id = 0
+        payment_brw = self.env["account.payment"].browse(payment_id)
+        print ' \n \n reconcileeeeeeeeeeeeeeeeeeeeeeeeee \n \n', payment_brw
+        for line in payment_brw.move_line_ids:
+            if line.credit > 0.00:
+                payment_credit_id = line.id
+        invoice_brw.assign_outstanding_credit(payment_credit_id)
+        invoice_brw.action_invoice_paid()
+        return True
 
-    #     res = self.payment_env.action_validate_invoice_payment()
-    #     print 'paaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaymeeeeeeeeeeeeeeeeeeeeeeeeeeent', res
-    #     return res
+    @api.model
+    def _create_payment(self):
+        payment_env = self.env['account.payment']
+        if self.currency_id.name == 'USD':
+            journal = 13
+        else:
+            journal = 11
+        payment_model = {
+                'payment_method_id': 1,
+                'payment_type': 'inbound',
+                'journal_id': journal,
+                'partner_type': 'customer',
+                'partner_id': self.partner_id.id,
+                'amount': self.amount_total,
+                'currency_id': self.currency_id.id,
+                'l10n_mx_edi_payment_method_id': self.l10n_mx_edi_payment_method_id.id,
+                'communication': self.number
+            }
+
+        res = payment_env.create(payment_model)
+        return res
 
     @api.multi
     def conekta_payment_validate(self):
@@ -133,7 +166,7 @@ class AccountInvoice(models.Model):
         try:
           charge  = conekta.Charge.create(conekta_object)
         except conekta.ConektaError as e:
-            # self.error = e.error_json['message_to_purchaser']
+            self.error = e.error_json['message_to_purchaser']
             self.communication ='Not Charge'
         else:
             return True
